fdde7597d5c2d040e9b5546847911436c0e53272
Carlos Augusto <caruros88@gmail.com>
2022-06-06 21:19:59 -0300

    iniciando o projeto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Alugaflix</title>
+        <script type="module" src="/js/app.js"></script>
+    </head>
+    <div id="app">
+    </div>
+</html>
