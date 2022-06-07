2bae13a6f93d2beb7df58cdc987e1d1574325b9d
Carlos Augusto <caruros88@gmail.com>
2022-06-06 22:51:44 -0300

    corrigindo relacionamento da model many to many

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -18,6 +18,6 @@ class Movie extends Model
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class);
+        return $this->belongsToMany(Tag::class, 'movie_tags', 'movie_id', 'tag_id');
     }
 }
