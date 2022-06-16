2fba0fde785edb8100b77ad1ba7cf1df14a44355
Carlos Augusto <caruros88@gmail.com>
2022-06-15 21:03:58 -0300

    criado resources para padronizar retorno das apis

---
+++ app/Http/Resources/MovieResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MovieResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+//            'file' => $this->file,
+            'size' => $this->size,
+            'tags' => !empty($this->tags) ? TagResource::collection($this->tags) : []
+        ];
+    }
+}
