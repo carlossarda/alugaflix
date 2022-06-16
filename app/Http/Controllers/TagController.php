6df8158a12deef1604c4eb1ce15627dc6edeb483
Carlos Augusto <caruros88@gmail.com>
2022-06-16 12:14:03 -0300

    função de criação de tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TagResource;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $params = $request->all();
+
+        if (!$request->has(['nome'])) {
+            return response([
+                "message" => "Wrong params"
+            ], 400);
+        }
+
+        $tagExists = Tag::where('name', $params['nome'])->first();
+
+        if (!empty($tagExists)) {
+            return response([
+                "message" => "Tag already exists"
+            ], 400);
+        }
+
+        $tag = new Tag();
+        $tag->name = $params['nome'];
+
+        $tag->saveOrFail();
+
+        return response(new TagResource($tag), 201);
+    }
 }
