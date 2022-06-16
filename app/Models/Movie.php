e831e0c9538b64f60a3e4d42a8dcdef9daf774de
Carlos Augusto <caruros88@gmail.com>
2022-06-16 12:40:50 -0300

    adicionado propriety na model

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property string name
+ * @property int size
+ * @property false|string file
+ * @property mixed id
+ */
 class Movie extends Model
 {
     use HasFactory, SoftDeletes;
