eeb5c85ba6cc016f7d0edd2e5667a322a2f4324f
Carlos Augusto <caruros88@gmail.com>
2022-06-15 23:16:38 -0300

    alterado para uma entidade comum

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Model
 {
     use HasFactory,SoftDeletes;
 
