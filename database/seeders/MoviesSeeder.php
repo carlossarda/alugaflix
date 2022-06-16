b8904643fd0fba2b113f5b7ec013a6f9d4ce45b5
Carlos Augusto <caruros88@gmail.com>
2022-06-16 12:13:32 -0300

    corrigindo seeder, por falta de data de criação

--- database/seeders/MoviesSeeder.php
+++ database/seeders/MoviesSeeder.php
@@ -16,26 +16,30 @@ class MoviesSeeder extends Seeder
     {
         DB::table("movies")->insert([
             "name" => "Um Sonho de Liberdade",
-            "file" => "/storage/um_sonho_de_liberdade.mp4",
-            "size" => "5000000"
+            "file" => "video_files/um_sonho_de_liberdade.mp4",
+            "size" => "5000000",
+            "created_at" => (new \DateTime())->format('Y-m-d H:i:s')
         ]);
 
         DB::table("movies")->insert([
             "name" => "O Poderoso Chefão",
-            "file" => "/storage/poderoso_chefao_1.mp4",
-            "size" => "4500000"
+            "file" => "video_files/poderoso_chefao_1.mp4",
+            "size" => "4500000",
+            "created_at" => (new \DateTime())->format('Y-m-d H:i:s')
         ]);
 
         DB::table("movies")->insert([
             "name" => "Batman: O Cavaleiro das Trevas",
-            "file" => "/storage/batman_cavaleiro_das_trevas.mp4",
-            "size" => "4800000"
+            "file" => "video_files/batman_cavaleiro_das_trevas.mp4",
+            "size" => "4800000",
+            "created_at" => (new \DateTime())->format('Y-m-d H:i:s')
         ]);
 
         DB::table("movies")->insert([
             "name" => "Pulp Fiction: Tempo de Violência",
-            "file" => "/storage/pulp_fiction.mp4",
-            "size" => "4891385"
+            "file" => "video_files/pulp_fiction.mp4",
+            "size" => "4891385",
+            "created_at" => (new \DateTime())->format('Y-m-d H:i:s')
         ]);
     }
 }
