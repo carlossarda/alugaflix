41d48ea4340d27871d176b705334768d0edd5685
Carlos Augusto <caruros88@gmail.com>
2022-06-16 12:40:33 -0300

    adicionado propriety na model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property string name
+ */
 class Tag extends Model
 {
     use HasFactory, SoftDeletes;
